ec6a6bb64c360c855e4bab66f93cff8d7eb31c55
Steven Miyakawa <steven.miyakawa@here.com>
2016-06-14 14:47:35 -0700

    Update React Hot Loader to latest version

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -51,7 +51,7 @@ module.exports = {
     loaders: [
       {
         test: /\.jsx?$/,
-        loaders: ['react-hot', 'babel'],
+        loaders: ['babel'],
         include: PATHS.app
       },
       {
--- src/js/containers/FriendListApp/FriendListApp.js
+++ src/js/containers/FriendListApp/FriendListApp.js
@@ -24,7 +24,7 @@ export class FriendListApp extends Component {
 
     return (
       <div className="friendListApp">
-        <h1>ddddddDa Bro's List</h1>
+        <h1>Da Bro's List</h1>
         <AddFriendInput addFriend={actions.addFriend} />
         <FriendList friends={friendsById} actions={actions} />
       </div>
